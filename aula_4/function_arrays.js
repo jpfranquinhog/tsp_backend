13b50552d006f0ed84567867a4ec938b7079c743
cenamaluco <jpfranquinhog@gmail.com>
2018-04-06 17:42:38 +0100

    c

--- aula_4/function_arrays.js
+++ aula_4/function_arrays.js
@@ -1,23 +1,19 @@
 function main(){
     var array=[];
-    var i;
-    var x= function(){
+    array.push(function(){
         console.log("world 1");
-    }
-    var y= function(){
+    });
+    array.push(function(){
         console.log("world 2");
-    }
-    var z= function(){
+    });
+    array.push(function(){
         console.log("world 3");
-    }
-    array.push(x());
-    array.push(y());
-    array.push(z());
+    });
     //for(i=0;i<array.length;i++){
     //    array[i];
     //}
     array.forEach(element => {
-        element;
+        element();
     });
 }
 
