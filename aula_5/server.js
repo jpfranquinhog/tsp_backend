cb4ab5a42c7b15c0e58fd9b036e03fc54f01ef19
cenamaluco <jpfranquinhog@gmail.com>
2018-04-18 20:42:06 +0100

    trabalho

--- aula_5/server.js
+++ aula_5/server.js
@@ -1,26 +1,71 @@
-const express = require('express');
-const app = express();
-const bodyParser = require("body-parser");
+/*var server = app.listen(3000, function(){
+    var host = server.address().address
+    var port = server.address().port
 
+    console.log("Example app listening at http://%s:%s", host, port)
+})*/
+
+const express = require('express');
+const bodyParser=require('body-parser');
+
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-var fs = require("fs");
-function readfile(name){
-    var file = fs.readFileSync(name,"utf-8");
+const fs = require('fs'); // modulo file system, que serve para apanhar, criar ficheiros ect
+const uuid = require("uuid/v1");
+function readFile(path){
+    var file = fs.readFileSync(path,'utf-8');
     return file;
 }
 
-var fileData = readfile("./persons.json");
+app.get('/', function(req, res){
+    var f = readFile("./videos.json");    
+    res.send(f);
+});
 
-app.get('/', (req, res) => res.send(fileData));
+app.post('/add',function(req,res){
+    var f=readFile("./videos.json");
+    var jsondata=JSON.parse(f); // transforma data "string" para um "objecto"
+    var size=Object.keys(jsondata).length;
+    var id=uuid();
+    var p=req.body;
+    p.id=id;
+    jsondata["video"+id]=p;
+    res.send(JSON.stringify(jsondata));
+    fs.writeFile("videos.json",JSON.stringify(jsondata), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+});
 
-app.post("/add", function(request,response){
-    var file = JSON.parse(fileData);
-    var id=Object.keys(file).length+1;
-    request.body.id = id;
-    file["person"+id]=request.body;
-    response.send(JSON.stringify(file));
-})
+app.delete('/del/:id',function(req,res){
+    var f=readFile("./videos.json");
+    var jsondata=JSON.parse(f); // transforma data "string" para um "objecto"
+    var size=Object.keys(jsondata).length;
+    var id=req.params.id;
+   
+    var video = jsondata["video" + id];
+
+    for (let i = 0; i < size; i++) {
+        vid="video"+i;
+        if(video!=undefined){
+            delete jsondata["video" + id];
+            res.send(JSON.stringify(jsondata));
+            fs.writeFile("videos.json",JSON.stringify(jsondata), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        }); 
+        }
+    }
+});
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+
